280c1c746a5017c5ac3096f60965940d45386d5f
Mark Struchkov <upagge@icloud.com>
2019-05-04 13:58:55 +0300

    Рефакторинг

--- src/main/java/org/sadtech/bot/autoresponder/saver/EmailConfig.java
+++ src/main/java/org/sadtech/bot/autoresponder/saver/EmailConfig.java
@@ -20,45 +20,53 @@ public final class EmailConfig {
         return password;
     }
 
+    public static Builder builder() {
+        return new EmailConfig().new Builder();
+    }
+
     public class Builder {
 
         private Builder() {
 
         }
 
-        public Builder setSmtpHost(String smtpHost) {
+        public Builder smtpHost(String smtpHost) {
             EmailConfig.this.props.setProperty("mail.smtp.host", smtpHost);
             return this;
         }
 
-        public Builder setSmtpSocketFactoryPort(Integer smtpSocketFactoryPortort) {
+        public Builder smtpSocketFactoryPort(Integer smtpSocketFactoryPortort) {
             EmailConfig.this.props.setProperty("mail.smtp.socketFactory.port", smtpSocketFactoryPortort.toString());
             return this;
         }
 
-        public Builder setSmtpSocketFactoryClass(String smtpSocketFactoryClass) {
+        public Builder smtpSocketFactoryClass(String smtpSocketFactoryClass) {
             EmailConfig.this.props.setProperty("mail.smtp.socketFactory.class", smtpSocketFactoryClass);
             return this;
         }
 
-        public Builder setSmtpAuth(Boolean smtpAuth) {
+        public Builder smtpAuth(Boolean smtpAuth) {
             EmailConfig.this.props.setProperty("mail.smtp.auth", (smtpAuth) ? "true" : "false");
             return this;
         }
 
-        public Builder setSmtpPort(Integer smtpPort) {
+        public Builder smtpPort(Integer smtpPort) {
             EmailConfig.this.props.setProperty("mail.smtp.port", smtpPort.toString());
             return this;
         }
 
-        public Builder setUsername(String username) {
+        public Builder email(String username) {
             EmailConfig.this.username = username;
             return this;
         }
 
-        public Builder setPassword(String password) {
+        public Builder password(String password) {
             EmailConfig.this.password = password;
             return this;
+        }
+
+        public EmailConfig build() {
+            return EmailConfig.this;
         }
 
     }
